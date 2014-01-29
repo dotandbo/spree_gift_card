23ba795ea8ebb168ce6a14efb195768162b07f79
Chris Clerville <chris@dotandbo.com>
2014-01-28 17:24:14 -0800

    updated scope syntax

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,7 +2,7 @@ Spree::Product.class_eval do
 
   #attr_accessible :is_gift_card
 
-  scope :gift_cards, where(is_gift_card: true)
-  scope :not_gift_cards, where(is_gift_card: false)
+  scope :gift_cards, -> { where(is_gift_card: true) }
+  scope :not_gift_cards, -> { where(is_gift_card: false) }
 
 end
