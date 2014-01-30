f376c4d156280e0036aebe585cf76feba0b4ab1a
Chris Clerville <chris@dotandbo.com>
2014-01-30 13:33:45 -0800

    use order_params instead

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::OrdersController.class_eval do
 
   durably_decorate :update , mode: 'soft' , sha: get_durably_sha( 'Spree::OrdersController#update' ) do
     @order = current_order
-    if @order.update_attributes(params[:order])
+    if @order.update_attributes(order_params)
       render :edit and return unless apply_coupon_code if defined?(Spree::Promo)
       render :edit and return unless apply_gift_code
 
