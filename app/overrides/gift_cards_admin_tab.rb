88399815cfde5c2037d64f477323f297f1db1082
Cyd La Luz <claluz@dotandbo.com>
2015-08-11 16:28:40 -0700

    - Adds classes to gift cards tab in spree/admin so that it aligns with other tabs

--- app/overrides/gift_cards_admin_tab.rb
+++ app/overrides/gift_cards_admin_tab.rb
@@ -1,4 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
+Deface::Override.new(:class => "fa fa-icon-file icon_link with-tip",
+                     :virtual_path => "spree/admin/shared/_menu",
                      :name => "gift_card_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab(:gift_cards) %>",
